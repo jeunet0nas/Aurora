89672a4845b83013bf980175687eb6df37b57ed6
j3un3t0nas <edgarust24@gmail.com>
2024-12-15 14:57:40 +0700

    first commit

---
+++ backend/app/Models/User.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'customer_name',
+        'email',
+        'password',
+        'city',
+        'province',
+        'address',
+        'phone_number',
+        'profile_img',
+        'profile_completed'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+
+    public function orders(){
+        return $this->hasMany(Order::class)->with('books')->latest();
+    }
+
+    public function image_path(){
+        if($this->profile_image){
+            return asset('storage/images/users'.$this->profile_image);
+        }else {
+            return 'https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_640.png';
+        }
+    }
+}
